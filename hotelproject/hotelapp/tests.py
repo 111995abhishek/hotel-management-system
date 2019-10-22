a103d7d8cd99a992e2c217bb79921ffeeabdf9d2
abhishek <111995abhishek@gmail.com>
2019-10-22 17:22:25 +0530

    validation and test cases

--- hotelproject/hotelapp/tests.py
+++ hotelproject/hotelapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Hotel,Rooms,Bookings
 
-# Create your tests here.
+class HotelTestCase(TestCase):
+    def setUp(self):
+        hotel=Hotel.objects.create(hotel_name='novotel',)
+    def test_case_check_hotel_name(self):
+        hotel=Hotel.objects.get(hotel_name='novotel',)
+
+class RoomsTestcase(TestCase):
+    rooms=Rooms.objects.create(room_type='Deluxe',room_no='price',Price='5000',no_of_members='2')
+
+
+
